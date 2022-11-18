8b80724f42b15d53a91cc5750cab556b3f2fcdad
alumnostlma <118541485+alumnostlma@users.noreply.github.com>
2022-11-18 07:54:17 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyCVO9gtoMsj9dtefe1BCQQ--4-RwSp3VKw",
+  authDomain: "alumnostlma-6d68d.firebaseapp.com",
+  projectId: "alumnostlma-6d68d",
+  storageBucket: "alumnostlma-6d68d.appspot.com",
+  messagingSenderId: "1088635488560",
+  appId: "1:1088635488560:web:9074ac7eccbd6746e260f5",
+  measurementId: "G-GJL05B36ZF"
 });
